3e7534ca0d27ae3a38a09ea2d7c31c6f573a5a4e
Navaneeth <knavaneethkumar8@gmail.com>
2025-12-30 17:32:32 +0530

    added fetch

--- scripts/signin.js
+++ scripts/signin.js
@@ -3,12 +3,14 @@ console.log('chack');
 const continueButton = document.querySelector('.js-continue-button');
 const emailInput = document.querySelector('.js-email-input');
 
-continueButton.addEventListener('click', () => {
+continueButton.addEventListener('click', async () => {
   console.log('sign in');
   const input = emailInput.value;
   if(input) {
     console.log(input);
     emailInput.value = '';
+    const result = await fetch('http://localhost:3500/');
+    console.log(`successfully fetched : ${result}`);
   } else {
     console.log('please enter email');
   }
