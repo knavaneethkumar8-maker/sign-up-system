31d56b7c7fda05e56d22ec0b2a1f3a41894ee6b9
Navaneeth <knavaneethkumar8@gmail.com>
2026-01-01 07:42:01 +0530

    added credentials and body and headers for register

--- scripts/signin.js
+++ scripts/signin.js
@@ -9,10 +9,22 @@ continueButton.addEventListener('click', async () => {
   if(input) {
     console.log(input);
     emailInput.value = '';
-    const result = await fetch('http://localhost:3500/index')
-                    .then((response) => {
-                      return response.text()
-                    });
+    const result = await fetch('http://localhost:3500/register', {
+      credentials : 'include',
+      method : 'POST',
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body : {
+        "username" : "Vetrimaran",
+        "email" : "nithinvenkatesh@gmail.com",
+        "pwd" : "222"
+      }
+    }).then((response) => {
+      return response.json();
+    }).catch((err) => {
+      console.log(err);
+    });
     console.log(`successfully fetched : ${ result}`);
   } else {
     console.log('please enter email');
